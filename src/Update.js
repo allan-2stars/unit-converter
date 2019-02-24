97477a7a7337052309f7021eb9e7636437474895
Allan Wang <wang.allan@outlook.com>
2019-02-24 16:15:42 +1100

    add converting function to Update. done the app for Temperature convert

--- src/Update.js
+++ src/Update.js
@@ -38,6 +38,83 @@ export function rightUnitChangedMessage(unitRight) {
   };
 }
 
+function round(number) {
+  return Math.round(number * 10) / 10;
+}
+
+// Converting Algorithm Function
+function convert(model) {
+  const { valueLeft, valueRight, unitLeft, unitRight } = model;
+
+  // if model.sourceLeft is true then use everything on Left
+  // otherwise use everything from right.
+  const [fromUnit, fromTemp, toUnit] = model.sourceLeft
+    ? [unitLeft, valueLeft, unitRight]
+    : [unitRight, valueRight, unitLeft];
+
+  const otherValue = R.pipe(
+    convertFromToTempValue,
+    round
+  )(fromUnit, toUnit, fromTemp);
+
+  return model.sourceLeft
+    ? { ...model, valueRight: otherValue }
+    : { ...model, valueLeft: otherValue };
+}
+
+//
+function convertFromToTempValue(fromUnit, toUnit, temp) {
+  const convertFn = R.pathOr(
+    R.identity, // Default value, return itself
+    // return the value of "fromUnit.toUnit" see below UnitConversions object
+    [fromUnit, toUnit],
+    // the object set
+    UnitConversions
+  );
+  console.log(temp);
+  return convertFn(temp);
+}
+
+function FtoC(temp) {
+  return (5 / 9) * (temp - 32);
+}
+
+function CtoF(temp) {
+  return (9 / 5) * temp + 32;
+}
+
+function KtoC(temp) {
+  return temp - 273.15;
+}
+
+function CtoK(temp) {
+  return temp + 273.15;
+}
+
+const FtoK = R.pipe(
+  FtoC,
+  CtoK
+);
+const KtoF = R.pipe(
+  KtoC,
+  CtoF
+);
+
+const UnitConversions = {
+  Celsius: {
+    Fahrenheit: CtoF,
+    Kelvin: CtoK
+  },
+  Fahrenheit: {
+    Celsius: FtoC,
+    Kelvin: FtoK
+  },
+  Kelvin: {
+    Celsius: KtoC,
+    Fahrenheit: KtoF
+  }
+};
+
 // convert string to a integer number
 const toInt = R.pipe(
   parseInt,
@@ -51,24 +128,29 @@ function update(msg, model) {
         return { ...model, sourceLeft: true, valueLeft: '', valueRight: '' };
       }
       const valueLeft = toInt(msg.valueLeft);
-      return { ...model, sourceLeft: true, valueLeft: valueLeft };
+      return convert({ ...model, sourceLeft: true, valueLeft: valueLeft });
     }
     //
     case messageTypes.RIGHT_VALUE_INPUT: {
       if (msg.valueRight === '') {
-        return { ...model, sourceLeft: false, valueLeft: '', valueRight: '' };
+        return {
+          ...model,
+          sourceLeft: false,
+          valueLeft: '',
+          valueRight: ''
+        };
       }
       const valueRight = toInt(msg.valueRight);
-      return { ...model, sourceLeft: false, valueRight: valueRight };
+      return convert({ ...model, sourceLeft: false, valueRight: valueRight });
     }
     //
     case messageTypes.LEFT_UNIT_CHANGED: {
       const { unitLeft } = msg;
-      return { ...model, unitLeft };
+      return convert({ ...model, unitLeft });
     }
     case messageTypes.RIGHT_UNIT_CHANGED: {
       const { unitRight } = msg;
-      return { ...model, unitRight };
+      return convert({ ...model, unitRight });
     }
   }
   return model;
