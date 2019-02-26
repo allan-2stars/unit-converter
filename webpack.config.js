adb9c45236db69d6c0685f9d289344226162292c
Allan Wang <wang.allan@outlook.com>
2019-02-26 15:02:36 +1100

    update to babel-core 7 and webpack to versoin 3.2.1

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ module.exports = {
   entry: ['./src/index.js'],
   devtool: 'inline-source-map',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   watch: true,
   devServer: {
     contentBase: './src',
     compress: true,
-    port: 9000,
+    port: 9000
   },
   module: {
     rules: [
@@ -18,11 +18,11 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['babel-preset-env'],
-            plugins: [require('babel-plugin-transform-object-rest-spread')],
-          },
-        },
-      },
-    ],
-  },
+            presets: ['@babel/preset-env'],
+            plugins: [require('babel-plugin-transform-object-rest-spread')]
+          }
+        }
+      }
+    ]
+  }
 };
